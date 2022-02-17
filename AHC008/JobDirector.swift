f4e5b695550f5141f564ddd07deba83f997fe973
Tatsuya Ishii <tishii2479@gmail.com>
2022-02-17 21:30:27 +0900

    Change grid layout

--- AHC008/JobDirector.swift
+++ AHC008/JobDirector.swift
@@ -49,30 +49,46 @@ class SquareGridJobDirector: JobDirector {
         for grid in grids { arr.append(grid.gate) }
         // Where horizontal block and vertical block intersects
         let intersections = [
-            Position(x: 11, y: 4),
-            Position(x: 18, y: 4),
-            Position(x: 6, y: 6),
-            Position(x: 12, y: 6),
-            Position(x: 17, y: 6),
-            Position(x: 23, y: 6),
-            Position(x: 6, y: 23),
-            Position(x: 12, y: 23),
-            Position(x: 17, y: 23),
-            Position(x: 23, y: 23),
-            Position(x: 11, y: 25),
-            Position(x: 18, y: 25),
-            Position(x: 4, y: 11),
-            Position(x: 4, y: 18),
-            Position(x: 25, y: 11),
-            Position(x: 25, y: 18),
-            Position(x: 6, y: 12),
-            Position(x: 6, y: 17),
-            Position(x: 12, y: 12),
-            Position(x: 17, y: 17),
-            Position(x: 12, y: 17),
-            Position(x: 17, y: 12),
-//            Position(x: 12, y: 14),
-//            Position(x: 17, y: 14),
+            Position(x: 5, y: 3),
+            Position(x: 11, y: 3),
+            Position(x: 18, y: 3),
+            Position(x: 24, y: 3),
+            Position(x: 3, y: 4),
+            Position(x: 26, y: 4),
+            Position(x: 5, y: 5),
+            Position(x: 9, y: 5),
+            Position(x: 13, y: 5),
+            Position(x: 15, y: 5),
+            Position(x: 24, y: 5),
+            Position(x: 15, y: 9),
+            Position(x: 24, y: 9),
+            Position(x: 3, y: 11),
+            Position(x: 26, y: 11),
+            Position(x: 15, y: 13),
+            Position(x: 24, y: 13),
+            Position(x: 5, y: 14),
+            Position(x: 9, y: 14),
+            Position(x: 13, y: 14),
+            Position(x: 16, y: 15),
+            Position(x: 20, y: 15),
+            Position(x: 24, y: 15),
+            Position(x: 5, y: 16),
+            Position(x: 14, y: 16),
+            Position(x: 3, y: 18),
+            Position(x: 26, y: 18),
+            Position(x: 5, y: 20),
+            Position(x: 14, y: 20),
+            Position(x: 5, y: 24),
+            Position(x: 14, y: 24),
+            Position(x: 16, y: 24),
+            Position(x: 20, y: 24),
+            Position(x: 24, y: 24),
+            Position(x: 3, y: 25),
+            Position(x: 26, y: 25),
+            Position(x: 5, y: 26),
+            Position(x: 11, y: 26),
+            Position(x: 18, y: 26),
+            Position(x: 24, y: 26),
         ]
         return arr + intersections
     }()
@@ -116,6 +132,74 @@ class SquareGridJobDirector: JobDirector {
 // MARK: SquareGridJobDirector.Assign
 
 extension SquareGridJobDirector {
+    private func assignGridJobsHorizontal(field: inout Field, humans: inout [Human], pets: inout [Pet]) {
+        var jobs = [Schedule.Job]()
+        var horizontalJobs = [Schedule.Job]()
+        
+        // Side horizontal
+        for y in [5, 11, 18, 24] {
+            horizontalJobs.append(
+                createLineBlockJob(from: Position(x: 0, y: y), to: Position(x: 3, y: y))
+            )
+            
+            horizontalJobs.append(
+                createLineBlockJob(from: Position(x: 29, y: y), to: Position(x: 26, y: y))
+            )
+        }
+        
+        // Top and bottom horizontal
+        for y in [5, 24] {
+            horizontalJobs.append(
+                createBlockJobWithMove(
+                    from: Position(x: 6, y: y),
+                    to: Position(x: 23, y: y),
+                    checkDirections: [.down, .up],
+                    skipBlocks: skipBlocks
+                )
+            )
+        }
+        
+        // Center sides horizontal
+        for y in [13, 15] {
+            horizontalJobs.append(
+                createLineBlockJob(from: Position(x: 7, y: y), to: Position(x: 11, y: y))
+            )
+            horizontalJobs.append(
+                createLineBlockJob(from: Position(x: 18, y: y), to: Position(x: 22, y: y))
+            )
+        }
+        
+        var hJobs = [[Schedule.Job]](repeating: [Schedule.Job](), count: 3)
+        for job in horizontalJobs {
+            guard let y = job.nextUnit?.pos.y else {
+                IO.log("Job start position does not exist for \(job)", type: .warn)
+                continue
+            }
+            hJobs[y / 10].append(job)
+        }
+        for i in 0 ..< 3 {
+            hJobs[i].sort(by: { jobA, jobB in
+                guard let aStart = jobA.nextUnit?.pos, let bStart = jobB.nextUnit?.pos else {
+                    IO.log("Job start position does not exist for \(jobA), \(jobB)", type: .warn)
+                    return false
+                }
+                return aStart.x < bStart.x
+            })
+            
+            var hJob = Schedule.Job(units: [])
+            for job in hJobs[i] {
+                hJob += job
+                if hJob.cost > costLimit {
+                    jobs.append(hJob)
+                    hJob = Schedule.Job(units: [])
+                }
+            }
+            jobs.append(hJob)
+        }
+        
+        assignJobs(jobs: jobs, humans: &humans)
+    }
+
     private func assignGridJobsVertical(field: inout Field, humans: inout [Human], pets: inout [Pet]) {
         var jobs = [Schedule.Job]()
         var verticalJobs = [Schedule.Job]()
@@ -185,80 +269,6 @@ extension SquareGridJobDirector {
 
         assignJobs(jobs: jobs, humans: &humans)
     }
-    
-    private func assignGridJobsHorizontal(field: inout Field, humans: inout [Human], pets: inout [Pet]) {
-        var jobs = [Schedule.Job]()
-        var horizontalJobs = [Schedule.Job]()
-        
-        // Side horizontal
-        for y in [5, 11, 18, 24] {
-            horizontalJobs.append(
-                createLineBlockJob(from: Position(x: 0, y: y), to: Position(x: 3, y: y))
-            )
-            
-            horizontalJobs.append(
-                createLineBlockJob(from: Position(x: 29, y: y), to: Position(x: 26, y: y))
-            )
-        }
-        
-        // Top and bottom horizontal
-        for y in [5, 24] {
-            horizontalJobs.append(
-                createBlockJobWithMove(
-                    from: Position(x: 6, y: y),
-                    to: Position(x: 23, y: y),
-                    checkDirections: [.down, .up],
-                    skipBlocks: skipBlocks
-                )
-            )
-        }
-        
-        // Center sides horizontal
-        for y in [12, 17] {
-            horizontalJobs.append(
-                createLineBlockJob(from: Position(x: 7, y: y), to: Position(x: 11, y: y))
-            )
-            horizontalJobs.append(
-                createLineBlockJob(from: Position(x: 18, y: y), to: Position(x: 22, y: y))
-            )
-        }
-        
-        // Center center horizontal
-//        horizontalJobs.append(
-//            createLineBlockJob(from: Position(x: 13, y: 14), to: Position(x: 16, y: 14))
-//        )
-        
-        
-        var hJobs = [[Schedule.Job]](repeating: [Schedule.Job](), count: 3)
-        for job in horizontalJobs {
-            guard let y = job.nextUnit?.pos.y else {
-                IO.log("Job start position does not exist for \(job)", type: .warn)
-                continue
-            }
-            hJobs[y / 10].append(job)
-        }
-        for i in 0 ..< 3 {
-            hJobs[i].sort(by: { jobA, jobB in
-                guard let aStart = jobA.nextUnit?.pos, let bStart = jobB.nextUnit?.pos else {
-                    IO.log("Job start position does not exist for \(jobA), \(jobB)", type: .warn)
-                    return false
-                }
-                return aStart.x < bStart.x
-            })
-            
-            var hJob = Schedule.Job(units: [])
-            for job in hJobs[i] {
-                hJob += job
-                if hJob.cost > costLimit {
-                    jobs.append(hJob)
-                    hJob = Schedule.Job(units: [])
-                }
-            }
-            jobs.append(hJob)
-        }
-        
-        assignJobs(jobs: jobs, humans: &humans)
-    }
 }
 
 // MARK: SquareGridJobDirector.Grid
@@ -267,54 +277,62 @@ extension SquareGridJobDirector {
     private func createGrid() {
         // Corners
         do {
-            let size = 5
-            grids.append(Grid(top: 0, left: 0, width: size, height: size, gate: Position(x: 4, y: 5)))
-            grids.append(Grid(top: 0, left: 25, width: size, height: size, gate: Position(x: 25, y: 5)))
-            grids.append(Grid(top: 25, left: 0, width: size, height: size, gate: Position(x: 4, y: 24)))
-            grids.append(Grid(top: 25, left: 25, width: size, height: size, gate: Position(x: 25, y: 24)))
+            let width = 5
+            let height = 4
+            grids.append(Grid(top: 0, left: 0, width: width, height: height, gate: Position(x: 4, y: 3)))
+            grids.append(Grid(top: 0, left: 25, width: width, height: height, gate: Position(x: 25, y: 3)))
+            grids.append(Grid(top: 26, left: 0, width: width, height: height, gate: Position(x: 4, y: 26)))
+            grids.append(Grid(top: 26, left: 25, width: width, height: height, gate: Position(x: 25, y: 26)))
         }
         
         // Sides
-        for y in [6, 12, 19] {
-            let width = 4
-            let height = y == 12 ? 6 : 5
-            grids.append(Grid(top: y, left: 0, width: width, height: height, gate: Position(x: 4, y: y + 2)))
-            grids.append(Grid(top: y, left: 26, width: width, height: height, gate: Position(x: 25, y: y + 2)))
+        for y in [5, 12, 19] {
+            let width = 3
+            let height = 6
+            grids.append(Grid(top: y, left: 0, width: width, height: height, gate: Position(x: 3, y: y + 2)))
+            grids.append(Grid(top: y, left: 27, width: width, height: height, gate: Position(x: 26, y: y + 2)))
         }
         
         // Top and bottom
         for x in [6, 12, 19] {
             let width = x == 12 ? 6 : 5
-            let height = 4
-            grids.append(Grid(top: 0, left: x, width: width, height: height, gate: Position(x: x + 2, y: 4)))
-            grids.append(Grid(top: 26, left: x, width: width, height: height, gate: Position(x: x + 2, y: 25)))
+            let height = 3
+            grids.append(Grid(top: 0, left: x, width: width, height: height, gate: Position(x: x + 2, y: 3)))
+            grids.append(Grid(top: 27, left: x, width: width, height: height, gate: Position(x: x + 2, y: 26)))
         }
         
-        // Center sides
-        for x in [7, 18] {
-            for y in [7, 13, 18] {
-                let width = 5
-                let height = y == 13 ? 4 : 5
-                let gateX = x == 7 ? 6 : 23
-                grids.append(Grid(top: y, left: x, width: width, height: height, gate: Position(x: gateX, y: y + 1)))
-            }
-        }
-        
-        // Center
+        // Center vertical
         do {
-            let width = 4
-            grids.append(Grid(top: 7, left: 13, width: width, height: 16, gate: Position(x: 14, y: 23)))
-//            grids.append(Grid(top: 15, left: 13, width: width, height: 8, gate: Position(x: 14, y: 23)))
+            let width = 3
+            let height = 8
+            grids.append(Grid(top: 6, left: 6, width: width, height: height, gate: Position(x: 7, y: 5)))
+            grids.append(Grid(top: 6, left: 10, width: width, height: height, gate: Position(x: 11, y: 5)))
+            
+            grids.append(Grid(top: 16, left: 17, width: width, height: height, gate: Position(x: 18, y: 24)))
+            grids.append(Grid(top: 16, left: 21, width: width, height: height, gate: Position(x: 22, y: 24)))
         }
+        
+        // Center horizontal
+        do {
+            let width = 8
+            let height = 3
+            grids.append(Grid(top: 17, left: 6, width: width, height: height, gate: Position(x: 5, y: 18)))
+            grids.append(Grid(top: 21, left: 6, width: width, height: height, gate: Position(x: 5, y: 22)))
+            
+            grids.append(Grid(top: 6, left: 16, width: width, height: height, gate: Position(x: 24, y: 7)))
+            grids.append(Grid(top: 10, left: 16, width: width, height: height, gate: Position(x: 24, y: 11)))
+        }
+        
+        dumpGrids(grids: grids)
     }
     
     // For Debug
     private func dumpGrids(grids: [Grid]) {
         var f = [[String]](repeating: [String](repeating: ".", count: fieldSize), count: fieldSize)
-        for (i, grid) in grids.enumerated() {
+        for grid in grids {
             for x in grid.topLeft.x ... grid.bottomRight.x {
                 for y in grid.topLeft.y ... grid.bottomRight.y {
-                    f[y][x] = String(i)
+                    f[y][x] = "Q"
                 }
             }
             f[grid.gate.y][grid.gate.x] = "!"
---
+++ Scripts/dots.py
@@ -0,0 +1,13 @@
+with open('v8.dt3') as f:
+    data: str = f.readline()
+    size, dots, palette, _ = data.split(':')
+    dots = dots.split(',')
+    assert len(dots) == 900
+    color = "104"
+    pos = []
+    for i in range(900):
+        if dots[i] == color:
+            pos.append(i)
+
+    for p in pos:
+        print(f'            Position(x: {p % 30}, y: {p // 30}),')
