e5b4240f2d45a1de0bd38705b5dababee241fcbd
Tatsuya Ishii <tishii2479@gmail.com>
2022-02-24 18:59:29 +0900

    Fix

--- AHC008/main.swift
+++ AHC008/main.swift
@@ -34,7 +34,7 @@ func main() {
         field: field,
         humans: humans,
         pets: pets,
-        gridManager: SquareGridManager()
+        gridManager: (M >= 9) ? SquareGridManagerV2() : SquareGridManager()
     )
     let manager = Manager(
         field: field,
